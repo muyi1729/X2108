a4610bc05ba8d35058c8261a49a272c07c15c1f7
muyi1729 <88604849+muyi1729@users.noreply.github.com>
2022-08-25 22:12:08 +0800

    Add files via upload

---
+++ js/member.js
@@ -0,0 +1,174 @@
+var member = [
+  {
+  	"phone": "No.01",
+    "name": "喻航"
+  },
+  {
+    "phone": "No.02",
+    "name": "宋辰宇"
+  },
+  {
+    "phone": "No.03",
+    "name": "隆姝瑶"
+  },
+  {
+    "phone": "No.04",
+    "name": "曾奕萌"
+  },
+  {
+    "phone": "No.04",
+    "name": "陈锦儒"
+  },
+  {
+    "phone": "No.04",
+    "name": "纵菀湘"
+  },
+  {
+    "phone": "No.04",
+    "name": "周子寒"
+  },
+  {
+    "phone": "No.04",
+    "name": "彭诗贻"
+  },
+  {
+    "phone": "No.04",
+    "name": "何旭"
+  },
+  {
+    "phone": "No.04",
+    "name": "曾语麒"
+  },
+  {
+    "phone": "No.04",
+    "name": "张凯盛"
+  },
+  {
+    "phone": "No.04",
+    "name": "蔡熊涵"
+  },
+  {
+    "phone": "No.04",
+    "name": "彭旭"
+  },
+  {
+    "phone": "No.04",
+    "name": "李瑾萱"
+  },
+  {
+    "phone": "No.04",
+    "name": "高煜鑫"
+  },
+  {
+    "phone": "No.04",
+    "name": "邓诗琪"
+  },
+  {
+    "phone": "No.04",
+    "name": "邹语琴"
+  },
+  {
+    "phone": "No.04",
+    "name": "吴瑞翔"
+  },
+  {
+    "phone": "",
+    "name": "刘金铭"
+  },
+  {
+    "phone": "",
+    "name": "高圆"
+  },
+  {
+    "phone": "",
+    "name": "范晨暄"
+  },
+  {
+    "phone": "",
+    "name": "高明骏"
+  },
+  {
+    "phone": "",
+    "name": "姚予萌"
+  },
+  {
+    "phone": "",
+    "name": "湛依萌"
+  },
+  {
+    "phone": "",
+    "name": "胡语忆"
+  },
+  {
+    "phone": "",
+    "name": "范豪杰"
+  },
+  {
+    "phone": "",
+    "name": "高思宇"
+  },
+  {
+    "phone": "",
+    "name": "周嘉宁"
+  },
+  {
+    "phone": "",
+    "name": "欧泽苒"
+  },
+  {
+    "phone": "",
+    "name": "黄熙程"
+  },
+  {
+    "phone": "",
+    "name": "高敏婷"
+  },
+  {
+    "phone": "",
+    "name": "管依菡"
+  },
+  {
+    "phone": "",
+    "name": "朱茗语"
+  },
+  {
+    "phone": "",
+    "name": "周向宇"
+  },
+  {
+    "phone": "",
+    "name": "宋紫瑞"
+  },
+  {
+    "phone": "",
+    "name": "刘子航"
+  },
+  {
+    "phone": "",
+    "name": "李鸿鑫"
+  },
+  {
+    "phone": "",
+    "name": "陈俊彦"
+  },
+  {
+    "phone": "",
+    "name": "邹舒涵"
+  },
+  {
+    "phone": "",
+    "name": "吴佳航"
+  },
+  {
+    "phone": "",
+    "name": "邹紫润"
+  },
+  {
+    "phone": "",
+    "name": "厉雅琳"
+  },
+  {
+    "phone": "",
+    "name": "邹秉航"
+  }
+]
